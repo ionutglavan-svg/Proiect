28f72fcce8443bcd65da540b2427e2e93ccaa336
ionutglavan-svg <ionut.glavan@cnodobescu.ro>
2025-12-24 19:24:34 +0000

    Am optimizat codul introducand o functie pt afisarea organizata a elementelor

--- parcurgere.sh
+++ parcurgere.sh
@@ -4,16 +4,76 @@ csv_file="$1"
 shift
 arr_cvs=() #declarare array
 
+pretty_print() {
+    array=("$@")  #pasam un array ca argument
+    i=0
+    for field in "${array[@]}" #adaugam o virgula la finalul elementelor de pe array
+    do
+        array[i]+=","
+        ((i++))
+    done
+    IFS="," read -r -a cuvinte <<< "${array[@]}" #cuvinte este un array cu toate field-urile din fisier ca elemente distincte
+    max=0
+    echo ${cuvinte[@]}
+    for cuv in "${cuvinte[@]}"
+    do
+    if [ ${#cuv} -gt $max ]
+    then
+        max=${#cuv}
+    fi
+    done
+    #mai sus am aflat numarul maxim de litere dintre toate cuvintele
+    j=0
+    while [ $j -lt ${#cuvinte[@]} ]
+    do
+        while [ $max -gt ${#cuvinte[$j]} ]
+        do
+            cuvinte[$j]+=" "
+        done
+        cuvinte[$j]+="|"
+        ((j++))
+    done
+    #mai sus am adaugat spatii si pipe uri fiecarui cuvant in functie de numarul maxim de litere
+    IFS="," read  -r -a cuv_0 <<< "${array[0]}" #am creeat un array ce contine toate cuvintele primei linii
+    x="${#cuv_0[@]}" #am retinut numarul de cuvinte (practic numarul de coloane din csv)
+
+    nr_linii=$(($x*($max+1)))
+
+    linii=""
+    i=0
+    while [ $i -le $nr_linii ]
+    do
+        linii+="-"
+        ((i++))
+    done
+    
+    #mai sus am creeat string ul cu linii delimitatoare
+    linie_act=()
+    j=0
+    while [ $j -le ${#cuvinte[@]} ]
+    do
+        if [ $x -gt 0 ]
+        then
+            linie_act+="${cuvinte[$j]}"
+            ((x--))
+        else
+            echo "$linie_act"
+            linie_act="${cuvinte[$j]}"
+            x=$((${#cuv_0[@]} - 1))
+            if [ $j -eq ${#cuv_0[@]} ]
+            then
+                echo $linii
+            fi
+        fi
+        ((j++))
+    done
+    return
+}
+
 while IFS="," read line
 do
     arr_cvs+=("$line") #se salveaza fiecare linie din fisier ca un element al array-ului
 done < "$csv_file"
-h=0
-while [ $h -lt ${#arr_cvs[@]} ]
-do
-    arr_cvs[$h]+=","
-    ((h++))
-done
 
 if [ "$1" = "--select" ] ; then
     shift
@@ -29,73 +89,17 @@ if [ "$1" = "--select" ] ; then
     #in locatie_col sunt salvate pozitiile pt elementele care trebuie afisate
     locatie_col=${locatie_col%,} #sterge ultima virgula din string: ${var%pattern} - sterge cel mai scurt pattern de la finalul string-ului
     afisare=$(cut -d "," -f${locatie_col} "$csv_file" | tail -n +1) 
-    echo "$afisare"
+    
+   while IFS= read -r line
+   do
+   array_afisare+=("$line")
+   done < <(echo "$afisare")
+
+    pretty_print "${array_afisare[@]}"
+
 fi
 if [ "$1" = "--select-all" ] ; then
-        
-
-    IFS="," read  -r -a cuvinte <<< "${arr_cvs[@]}"
-
-    # echo "${cuvinte[@]}"  #am creeat un array format din cuvintele fisierului
-    max=0
-    for cuv in "${cuvinte[@]}"
-    do
-    if [ ${#cuv} -gt $max ]
-    then
-        max=${#cuv}
-    fi
-    done
-    # echo $max 
-    #mai sus am aflat numarul maxim de litere dintre toate cuvintele
-
-    j=0
-    while [ $j -lt ${#cuvinte[@]} ]
-    do
-         #echo "${cuvinte[$j]}"
-        
-        while [ $max -gt ${#cuvinte[$j]} ]
-        do
-            cuvinte[$j]+=" "
-        done
-        cuvinte[$j]+="|"
-        ((j++))
-    done
-
-    #mai sus am adaugat spatii si pipe uri fiecarui cuvant in functie de numarul maxim de litere
-    IFS=", " read  -r -a cuv_0 <<< "${arr_cvs[0]}" #am creeat un array ce contine toate cuvintele primei linii
-    x="${#cuv_0[@]}" #am retinut numarul de cuvinte (practic numarul de coloane din csv)
-
-    nr_linii=$(($x*($max+1)))
-
-    linii=""
-        i=0
-        while [ $i -le $nr_linii ]
-        do
-        linii+="-"
-        ((i++))
-        done
-    
-    #mai sus am creeat string ul cu linii delimitatoare
-    cuv_act=()
-    j=0
-    while [ $j -le ${#cuvinte[@]} ]
-    do
-        if [ $x -gt 0 ]
-        then
-        cuv_act+="${cuvinte[$j]}"
-        ((x--))
-        else
-        echo "$cuv_act"
-        cuv_act="${cuvinte[$j]}"
-        x=$((${#cuv_0[@]} - 1))
-        if [ $j -eq ${#cuv_0[@]} ]
-        then
-        echo $linii
-        fi
-        fi
-        ((j++))
-    done
-    #mai sus am luat pe rand fiecare cuvant si l am concatenat astfel inacat sa se obtina numarul total de coloane (repetat)
+        pretty_print "${arr_cvs[@]}"
 fi
 if [ "$1" = "--validate" ] ; then
     header="${arr_cvs[0]}"
